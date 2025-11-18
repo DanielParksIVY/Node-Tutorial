4f0228d19bfa1f412b73cf778abdad2c9daa669e
DanielParksIVY <157433705+DanielParksIVY@users.noreply.github.com>
2025-11-17 20:47:54 -0500

    Update main.js

--- Node Project/public/js/main.js
+++ Node Project/public/js/main.js
@@ -1,9 +1,3 @@
-document.getElementById('apiBtn').addEventListener('click', async () => {
-    try {
-        const response = await fetch('/api');
-        const data = await response.json();
-        document.getElementById('apiMessage').textContent = data.message;
-    } catch (err) {
-        console.error(err);
-    }
+document.getElementById('apiBtn').addEventListener('click', () => {
+    document.getElementById('apiMessage').textContent = "Hello from static GitHub Pages!";
 });
