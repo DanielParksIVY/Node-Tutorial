b28f80d8837007b95e3333854623fcf3b47aef71
DanielParksIVY <157433705+DanielParksIVY@users.noreply.github.com>
2025-11-17 21:28:27 -0500

    Delete Node Project/public directory

--- Node Project/public/js/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('apiBtn').addEventListener('click', async () => {
-    try {
-        const response = await fetch('/api');
-        const data = await response.json();
-        document.getElementById('apiMessage').textContent = data.message;
-    } catch (err) {
-        console.error(err);
-    }
-});
